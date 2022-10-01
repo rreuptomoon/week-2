3934980d09df0cbe5318c95e9155175ab66398be
Rextin <fungtinrex04@gmail.com>
2022-10-01 17:43:08 +0800

    1- 5 answer done 1/10 17:42

--- Answer2.py
+++ Answer2.py
@@ -8,18 +8,9 @@ def avg(data):
         if i['manager'] == False:
              average_salary += i['salary']
              flag +=1
-        elif i["name"] == "John":
-            flag = False
-            average_salary += i['salary']
-            flag +=1
-        elif i["salary"] > 60000:
-            flag = False
-            average_salary += i['salary']
-            flag +=1
 
     return average_salary/flag
          
-
 # 請用你的程式補完這個函式的區塊
 res=avg({
 "employees":[
