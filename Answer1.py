090f081be37cbd11d9952027e98bde7b23b092e4
Rextin <fungtinrex04@gmail.com>
2022-10-01 12:29:41 +0800

    Answer 1 done

--- Answer1.py
+++ Answer1.py
@@ -1,13 +1,14 @@
 #Answer 1:
+
 def calculate(min,max,step):
-    sums=0 
-    for i in range(min,max+1,step):
-        sums += i 
-        if(sums != 6 and sums != 18 and sums != 0  ):
-           continue  
-        print("result = ",sums)
-        
-    
+    sums=0
+    i=min
+    while i<=max:
+        sums+=i
+        i+=step
+    print(sums)#
+                     
 calculate(1,3,1)
 calculate(4,8,2)
 calculate(-1,2,2)
+calculate(1,6,1)
